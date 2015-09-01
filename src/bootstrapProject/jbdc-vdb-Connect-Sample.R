b04e03f5ba9ca33434ad8cb5cfb7af9a4d27798c
Ludovic Claude <ludovic.claude54@gmail.com>
2015-09-01 15:05:30 +0200

    Use the R package

--- src/bootstrapProject/jbdc-vdb-Connect-Sample.R
+++
@@ -1,6 +0,0 @@
-library(RJDBC)
-drv <- JDBC("com.denodo.vdp.jdbc.Driver",
-           "/home/neuroinfo/lib/jdbc/vdp-jdbcdriver-core/denodo-vdp-jdbcdriver.jar",
-           identifier.quote="`")
-conn <- dbConnect(drv, "jdbc:vdb://localhost:9999/hbp_chuv", "admin", "admin")
-dbGetQuery(conn, "select count(*) from patient")
