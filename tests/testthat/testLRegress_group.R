5a1bc1899db93b76fce10f7ef42d448340d87d87
Ludovic Claude <ludovic.claude54@gmail.com>
2016-03-18 11:04:29 +0100

    Add test over 4 datasets

--- tests/testthat/testLRegress_group.R
+++ tests/testthat/testLRegress_group.R
@@ -37,11 +37,51 @@ test_that("Federated results are consistent with global results", {
     betaf <-  xestf[[1]];
     Sigmaf <- xestf[[2]];
 
-    gestf <- LRegress(data, ycolumn, Acolumns);
+    gestf <- LRegress(data, ycolumn, Acolumns, c());
 
     gbetaf <- gestf[[1]];
     gSigmaf <- gestf[[2]];
 
     expect_equivalent(betaf, gbetaf);
     expect_equivalent(Sigmaf, gSigmaf);
+});
+
+test_that("Federated results are valid over 4 disjoint datasets", {
+
+  library("MASS");
+  
+  N=20;
+  M=20;
+  
+  A1<-matrix(rnorm(N*M,mean=10,sd=4), N, M);
+  A2<-matrix(rnorm(N*M,mean=12,sd=5), N, M);
+  A3<-matrix(rnorm(N*M,mean=12,sd=3), N, M);
+  A4<-matrix(rnorm(N*M,mean=12,sd=3.5), N, M);
+  
+  x<-matrix(rnorm(M,mean=2,sd=1),M,1);
+  
+  y1<-A1%*%x;
+  y2<-A2%*%x;
+  y3<-A3%*%x;
+  y4<-A4%*%x;
+  
+  # Running regression at each local node.
+  xest1<-LRegress_Node(y1,A1);
+  xest2<-LRegress_Node(y2,A2);
+  xest3<-LRegress_Node(y3,A3);
+  xest4<-LRegress_Node(y4,A4);
+  
+  beta1<-xest1[[1]];Sigma1<-xest1[[2]];
+  beta2<-xest2[[1]];Sigma2<-xest2[[2]];
+  beta3<-xest3[[1]];Sigma3<-xest3[[2]];
+  beta4<-xest4[[1]];Sigma4<-xest4[[2]];
+  
+  
+  betas<-list(beta1,beta2,beta3,beta4);
+  Sigmas<-list(Sigma1,Sigma2,Sigma3,Sigma4);
+  # Integrating results of all Local Nodes in the Federation node.
+  xestf<-LRegress_Federation(betas,Sigmas);
+  betaf<-xestf[[1]];
+  Sigmaf<-xestf[[2]];
+
 });
